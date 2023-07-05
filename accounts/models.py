6f22ba193bd52ee573d9e429b95e905715135a59
han-mg <alrb1277@gmail.com>
2023-07-05 17:50:46 +0900

    create User Class

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,16 @@
-from django.contrib.auth.models import {
-	AbstractBaseUser, BaseUserManager, AbstractUser
-}
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, AbstractUser, models
+)
 
 class User(AbstractBaseUser):
-    email = models.EmaiField(max_length=255, unique = True)
-    nickname = models.CharField(max_length=255, blank=True, null=True)
-    active = models.BooleanField(default=True) # 로그인 할 수 있나?(휴면계정이 아닌가?)
-    staff = models.BooleanField(default=False) # superuser가 아닌 staff(김현준x 정택원o)
-    admin = models.BOoleanField(default=False) # superuser(김현준, 주영민)
-    
-    USERNAME_FIELD = 'email' # username을 email로 사용하겠다.
-    REQUIRED_FIELDS = ['nickname']
-# Create your models here.
-def __str__(self):
-    return self.email
+   email = models.EmailField(max_length=255, unique = True)
+   nickname = models.CharField(max_length=255, blank=True, null=True)
+   active = models.BooleanField(default=True) # 로그인 할 수 있나?(휴면계정이 아닌가?)
+   staff = models.BooleanField(default=False) # superuser가 아닌 staff(김현준x 정택원o)
+   admin = models.BooleanField(default=False) # superuser(김현준, 주영민)
 
+   USERNAME_FIELD = 'email' # username을 email로 사용하겠다.
+   REQUIRED_FIELDS = []
+   def __str__(self):
+        return self.email
+    
